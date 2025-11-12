be9d729232d35c2252989ad424b6ef22ee658e0c
HavilMal <aberetm@onet.eu>
2025-11-12 20:57:51 +0100

    Created constants for styling

--- app/src/main/java/com/plantCare/plantcare/ui/theme/Theme.kt
+++ app/src/main/java/com/plantCare/plantcare/ui/theme/Theme.kt
@@ -9,7 +9,9 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
@@ -258,23 +260,28 @@ val unspecified_scheme = ColorFamily(
 fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable() () -> Unit
 ) {
   val colorScheme = when {
-//      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-//          val context = LocalContext.current
-//          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-//      }
+     dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+         val context = LocalContext.current
+         if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+     }
       
       darkTheme -> darkScheme
       else -> lightScheme
   }
-
+    CompositionLocalProvider(
+        LocalSize provides Size(),
+        LocalSpacing provides Spacing(),
+    ) {
   MaterialTheme(
     colorScheme = colorScheme,
     typography = Typography,
     content = content
   )
+    }
+
 }
 
